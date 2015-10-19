403400f83a121836144f8a3c04b9b9d3b78b699c
zendy <zendywongso@gmail.com>
2015-10-19 15:07:18 +1100

    Add input box with event handler

--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,48 @@
 import React, {PropTypes} from 'react';
+import _ from 'lodash';
 
 const log = require('bows')('App');
 
 import styles from './index.css';
 
-const colors = [
-  {name: 'red', score: 1,},
-  {name: 'yellow', score: 2,}
+const factions = [
+  {color: 'red', name:'military', score: 0},
+  {color: 'brown', name:'coin', score: 1},
+  {color: 'grey', name:'wonder', score: 0},
+  {color: 'yellow', name:'commerce', score: 1},
+  {color: 'blue', name:'civilian', score: 0},
+  {color: 'purple', name:'guild', score: 0},
+  {color: 'green', name:'science', score: 0},
 ];
 
+function handleScoreChange(name, evt) {
+  const faction = _.findWhere(factions, {name: name});
+  faction.score = Number(evt.target.value) || 0;
+  render(factions);
+}
+
 export default class App extends React.Component {
+
   render() {
     return (
       <div className={styles.App}>
         <h1>THE NEW APP</h1>
-        {colors.map((color) => <span>{color.name} {color.score}</span>)}
+        <table>
+          {this.props.data.map((faction) => (
+            <tr>
+              <td>{faction.name}</td>
+              <td><input onChange={handleScoreChange.bind(null, faction.name)} type="text" value={faction.score} /></td>
+            </tr>
+          ))}
+          <tfooter>
+            <tr>
+              <td>Total</td>
+              <td>{this.props.data.reduce((val, item) => {
+                return val + item.score;
+              }, 0)}</td>
+            </tr>
+          </tfooter>
+        </table>
       </div>
     );
   }
@@ -22,4 +50,8 @@ export default class App extends React.Component {
 
 log('app starting up...');
 
-React.render(<App/>, document.body);
+function render(data) {
+  React.render(<App data={data}/>, document.body);
+}
+
+render(factions);
