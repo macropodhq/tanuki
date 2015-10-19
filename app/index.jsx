22a197c73aa9be9299852a8127f0c10b5bacd623
Nathan Hutchison <nathan@bugherd.com>
2015-10-19 14:38:45 +1100

    little bit of ui

--- app/index.jsx
+++ app/index.jsx
@@ -4,11 +4,17 @@ const log = require('bows')('App');
 
 import styles from './index.css';
 
+const colors = [
+  {name: 'red', score: 1,},
+  {name: 'yellow', score: 2,}
+];
+
 export default class App extends React.Component {
   render() {
     return (
       <div className={styles.App}>
         <h1>THE NEW APP</h1>
+        {colors.map((color) => <span>{color.name} {color.score}</span>)}
       </div>
     );
   }
